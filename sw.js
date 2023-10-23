e6a4215b9f039a50a8e9081bb0b136cfb3c2bb5a
Emc54 <emilioschristou1@gmail.com>
2023-10-24 00:16:19 +0100

    Site Update - Automatically from main

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1698094057';
+const cacheName = 'chirpy-1698102979';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
